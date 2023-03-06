887276cd203ee3c4494c773211583070b07444a3
Md My Zihad <123291403+mdmyzihad2400@users.noreply.github.com>
2023-03-06 10:26:41 +0600

    upload html css js file
    this is my best personal portfolio website

---
+++ script.js
@@ -0,0 +1,8 @@
+// =-====Typing Animation=======//
+
+var typed = new Typed(".typing",{
+  strings:["","Web Designer", "Web Developer", "Graphic Designer", "YouTuber", "Computer Programmer"],
+  typeSpeed:100,
+  BackSpeed:60,
+  loop:true
+})
